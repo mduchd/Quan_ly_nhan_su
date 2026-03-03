7ff52b86d8a108d46691d162c06586b361c9a9a3
mduchd <minhduchd20059b@gmail.com>
2026-03-03 22:38:57 +0700

    add project folder

---
+++ Quan_ly_nhan_su/BUS/NhanVienBUS.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quan_ly_nhan_su.BUS
+{
+    internal class NhanVienBUS
+    {
+    }
+}
---
+++ Quan_ly_nhan_su/GUI/ucBangLuong.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Quan_ly_nhan_su.GUI
+{
+    public partial class ucBangLuong : UserControl
+    {
+        public ucBangLuong()
+        {
+            InitializeComponent();
+        }
+    }
+}
